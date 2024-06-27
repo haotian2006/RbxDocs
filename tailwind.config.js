ab0df9036ac7cf00b022abe3249e48ef02e046c5
cyrus01337 <cyrus21337@gmail.com>
2024-06-27 02:25:02 +0000

    Whoops that's from my project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
-import daisyUI from "daisyui";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    daisyui: {
-        themes: ["light"],
-    },
     theme: {
         extend: {
             fontFamily: {
@@ -14,5 +10,4 @@ export default {
             },
         },
     },
-    plugins: [daisyUI],
 };
