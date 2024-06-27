5dc35c4387052759b0e2148cdeef8e702203d937
cyrus01337 <cyrus21337@gmail.com>
2024-06-27 02:27:48 +0000

    Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "MyDocs",
-  server: {
-    port: 3000,
-    host: true
-  },
-  site: "https://haotian2004.github.io",
-  integrations: [tailwind()]
+    base: "MyDocs",
+    server: {
+        port: 3000,
+        host: true,
+    },
+    site: "https://haotian2004.github.io",
+    integrations: [tailwind()],
 });
