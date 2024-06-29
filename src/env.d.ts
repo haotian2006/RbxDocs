ecf236857c90bc33212b30c86dc9f7788dc4baf6
cyrus01337 <cyrus21337@gmail.com>
2024-06-29 06:06:39 +0000

    Resolve present merge conflicts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-/// <reference types="astro/client" />
-/// <reference path="../.astro/types.d.ts" />
-=======
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
->>>>>>> e6c652fb53740fb24e253ac02b52398ca38f9966
