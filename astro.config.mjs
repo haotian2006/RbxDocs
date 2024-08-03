4f394c1c2d470b57803d52c381500d60dc1fed32
Haotian2006 <haotian2090@gmail.com>
2024-08-02 21:33:49 -0700

    Format docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "MyDocs",
-  server: {
-    port: 3000,
-    host: true
-  },
-  site: "https://haotian2006.github.io",
-  integrations: [tailwind(), react(), mdx(), icon()]
+    base: "MyDocs",
+    server: {
+        port: 3000,
+        host: true,
+    },
+    site: "https://haotian2006.github.io",
+    integrations: [tailwind(), react(), mdx(), icon()],
 });
