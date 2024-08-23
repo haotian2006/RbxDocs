70a277f5e05648f87e3babdde8a3ea7cec49352a
Haotian2006 <haotian2090@gmail.com>
2024-08-23 14:49:24 -0700

    Apply changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,4 @@
+import { Icon } from "astro-icon/components";
 import { defineCollection, z as zod } from "astro:content";
 
 const documents = defineCollection({
@@ -5,12 +6,7 @@ const documents = defineCollection({
     schema: zod.object({
         title: zod.string(),
         search: zod.boolean().default(true),
-        thumbnail: zod
-            .string()
-            .default(
-                "https://media.discordapp.net/attachments/1273764068775165992/1276341773521653842/New_Project.png?ex=66c92d68&is=66c7dbe8&hm=6054564bb1cae9c93a76cd0a1ba8d44667cfaf6ba46a4d5529a79096df17d37a&=&format=webp&quality=lossless&width=767&height=767",
-            ),
-
+        thumbnail: zod.string().default("/src/icons/thumbnails/WorkInProgress.webp"),
         author: zod
             .string()
             .regex(/(?:discord|github): ([0-9]+)/)
