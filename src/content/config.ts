82d214b4404aa6885cfb75b1e154e4e858fbb64b
Haotian2006 <haotian2090@gmail.com>
2024-09-12 12:52:20 -0700

    Change some colors

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const documents = defineCollection({
     schema: zod.object({
         title: zod.string(),
         search: zod.boolean().default(true),
-        thumbnail: zod.string().default("/src/icons/thumbnails/WorkInProgress.webp"),
+        thumbnail: zod.string().default("/images/thumbnails/WorkInProgress.webp"),
         author: zod
             .string()
             .regex(/(?:discord|github): ([0-9]+)/)
